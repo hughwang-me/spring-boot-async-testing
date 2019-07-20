8ecad2e543eef91738780ce08313f5f93a9df982
Hugh Wang <18501667737@163.com>
2019-07-21 00:04:55 +0800

    bybash

--- src/main/java/com/uwjx/springbootasynctesting/service/MulitTaskService.java
+++ src/main/java/com/uwjx/springbootasynctesting/service/MulitTaskService.java
@@ -6,17 +6,18 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.concurrent.Future;
 
 @Service
 @Slf4j
 public class MulitTaskService {
+    @Resource
+    private PointService pointService;
 
-    @Async("taskExecutor")
-    public Future<String> taskOne(int index) throws Exception{
-        log.error("任务 {} | 所在线程： {} 开始时间:{}"  , index ,Thread.currentThread() , DateUtil.current());
-        Thread.sleep(10000);
-        log.warn("任务 {} | 所在线程： {} 结束时间:{}"  , index , Thread.currentThread() , DateUtil.current());
-        return new AsyncResult<>("任务执行完毕");
+    public void task(int index) throws Exception{
+        pointService.taskOne(index);
     }
+
+
 }
---
+++ src/main/java/com/uwjx/springbootasynctesting/service/PointService.java
@@ -0,0 +1,22 @@
+package com.uwjx.springbootasynctesting.service;
+
+import com.uwjx.commons.utils.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Future;
+
+@Service
+@Slf4j
+public class PointService {
+
+    @Async("taskExecutor")
+    public Future<String> taskOne(int index) throws Exception{
+        log.error("任务 {} | 所在线程： {} 开始时间:{}"  , index ,Thread.currentThread() , DateUtil.current());
+        Thread.sleep(10000);
+        log.warn("任务 {} | 所在线程： {} 结束时间:{}"  , index , Thread.currentThread() , DateUtil.current());
+        return new AsyncResult<>("任务执行完毕");
+    }
+}
